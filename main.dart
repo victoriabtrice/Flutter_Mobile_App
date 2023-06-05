db775dceb98046f72d7e87dac7077ec4bc92be43
victoriabtrice <114051741+victoriabtrice@users.noreply.github.com>
2023-06-05 13:02:40 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
-// import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/my_creation.dart';
+import 'package:flutter_application_2/notif_settings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,9 +23,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'BurgeRush'),
+      home: const MyHomePage(title: 'Vlock'),
     );
   }
 }
@@ -84,23 +82,24 @@ class _MyHomePageState extends State<MyHomePage> {
           padding: EdgeInsets.zero,
           children: [
             SizedBox(
-              height: 130,
+              height: 160,
               child: DrawerHeader(
-                decoration: BoxDecoration(
-                color: Colors.amber[300],
+                decoration: const BoxDecoration(
+                color: Colors.blue,
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('BurgeRush', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
+                  children: const [
+                    Text('Vlock', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Color.fromARGB(255, 220, 200, 192)),),
                     SizedBox(height: 20,),
                     Text(
                       'Victoria Beatrice', 
-                      style: TextStyle(fontWeight: FontWeight.bold,),
+                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
                     ),
                     SizedBox(height: 3,),
                     Text(
                       'victoriabtre@gmail.com',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ],
                 )
@@ -123,26 +122,33 @@ class _MyHomePageState extends State<MyHomePage> {
             // ),
             ListTile(
               leading: const Icon(Icons.manage_accounts),
-              title: const Text('Manage Your Account'),
+              title: const Text('Manage Account'),
               onTap: () {
                 
               },
             ),
             ListTile(
-              leading: const Icon(Icons.fastfood_outlined),
-              title: const Text('My Creations'),
+              leading: const Icon(Icons.notifications_active),
+              title: const Text('Notification Settings'),
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => MyCreation()));
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const NotifSettPage()));
               },
             ),
-            ListTile(
-              leading: const Icon(Icons.receipt_long),
-              title: const Text('My Order'),
-              onTap: () {
+            // ListTile(
+            //   leading: const Icon(Icons.fastfood),
+            //   title: const Text('My Creations'),
+            //   onTap: () {
+            //     Navigator.push(context, MaterialPageRoute(builder: (context) => const MyCreation()));
+            //   },
+            // ),
+            // ListTile(
+            //   leading: const Icon(Icons.receipt_long),
+            //   title: const Text('My Order'),
+            //   onTap: () {
                 
-              },
-            ),
-            Divider(),
+            //   },
+            // ),
+            const Divider(),
             ListTile(
               leading: const Icon(Icons.help),
               title: const Text('Help'),
@@ -153,7 +159,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const AboutListTile (
               icon: Icon(Icons.info),
               // applicationIcon: Icon(),
-              applicationName: 'BurgeRush',
+              applicationName: 'Vlock',
               applicationVersion: '1.0.0',
               applicationLegalese: '© 2023 Company',
               aboutBoxChildren: [
@@ -164,114 +170,34 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         )
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Invoke "debug painting" (press "p" in the console, choose the
-              // "Toggle Debug Paint" action from the Flutter Inspector in Android
-              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-              // to see the wireframe for each widget.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Top Creation of the Week', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0), textAlign: TextAlign.left,),
-                SizedBox(height: 5.0,),
-                Container(
-                  decoration: BoxDecoration(color: Colors.amber[200]),
-                  width: MediaQuery.of(context).size.width*0.95,
-                  height: MediaQuery.of(context).size.height*0.2,
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(8.0),
-                        width: MediaQuery.of(context).size.width*0.4,
-                        height: MediaQuery.of(context).size.height*0.18,
-                        child: Image.asset('lib/assets/american-burger.jpg', fit: BoxFit.cover,),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Text('Rose Bites', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold), ),
-                            Text('by Rosette'),
-                            SizedBox(height: 6.0,),
-                            Text('Likes : 18', style: TextStyle(fontSize: 17.0),),
-                            Text('Price : Rp 25,000.-', style: TextStyle(fontSize: 17.0),),
-                            SizedBox(height: 5.0,),
-                            ElevatedButton(onPressed: (){}, child: Text('View Detail')),
-                            SizedBox(height: 4.0,)
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                )
-                // // Carousel
-                //   Container(
-                //     padding: const EdgeInsets.all(0.0),
-                //     child: CarouselSlider(
-                //       options: CarouselOptions(
-                //         height: 200.0,
-                //         enlargeCenterPage: false,
-                //         autoPlay: true,
-                //         aspectRatio: 16/9,
-                //         // autoPlayCurve: Curves.fastOutSlowIn,
-                //         enableInfiniteScroll: true,
-                //         autoPlayAnimationDuration: const Duration(milliseconds: 300),
-                //         viewportFraction: 1.0,
-                //       ),
-                //       items: [
-                //         // 1st Image of Slider
-                //         Container(
-                //           margin: const EdgeInsets.all(0.0),
-                //           decoration: const BoxDecoration(
-                //             image: DecorationImage(
-                //               image: AssetImage('lib/assets/home.c1.png'),
-                //               fit: BoxFit.cover,
-                //             )
-                //           ),
-                //         ),
-                //         // 2nd Image of Slider
-                //         Container(
-                //           margin: const EdgeInsets.all(0.0),
-                //           decoration: const BoxDecoration(
-                //             image: DecorationImage(
-                //               image: AssetImage('lib/assets/home.c2.1.png'),
-                //               fit: BoxFit.cover,
-                //             )
-                //           ),
-                //         ),
-                //         // 3rd Image of Slider
-                //         Container(
-                //           margin: const EdgeInsets.all(0.0),
-                //           decoration: const BoxDecoration(
-                //             image: DecorationImage(
-                //               image: AssetImage('lib/assets/home.c3.png'),
-                //               fit: BoxFit.cover,
-                //             )
-                //           ),
-                //         ),
-                //     ],)
-                //   ),
-              ],
-            ),
-          ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // const Text(
+            //   'You have pushed the button this many times:',
+            // ),
+            // Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headlineMedium,
+            // ),
+          ],
         ),
       ),
       // floatingActionButton: FloatingActionButton(
