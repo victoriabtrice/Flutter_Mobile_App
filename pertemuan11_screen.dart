f3aabe123487333730d1b0a35a2bed6a6fbd5920
victoriabtrice <114051741+victoriabtrice@users.noreply.github.com>
2023-06-12 13:15:00 +0700

    Add files via upload

---
+++ pertemuan11_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pertemuan11/pertemuan11_provider.dart';
+import 'package:provider/provider.dart';
+
+class Pertemuan11Screen extends StatefulWidget {
+  const Pertemuan11Screen({super.key});
+
+  @override
+  State<Pertemuan11Screen> createState() => _Pertemuan11ScreenState();
+}
+
+class _Pertemuan11ScreenState extends State<Pertemuan11Screen> {
+  @override
+  void initState(){
+    Future.microtask(() => Provider.of<Pertemuan11Provider>(context, listen: false).initialData());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pertemuan 11'),
+        actions: [
+          menuList(context)
+        ],
+      ),
+      body: body(context),
+    );
+  }
+
+  body(BuildContext context){
+    final prov = Provider.of<Pertemuan11Provider>(context);
+    if(prov.data == null){
+      return const CircularProgressIndicator();
+    }
+    else {
+      return ListView(
+        children: List.generate(prov.data['data']!.length, (index){
+          var item = prov.data['data']![index];
+          return Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(backgroundImage: NetworkImage(item['img'])),
+                title: Text(item['model']),
+              ),
+              const Divider()
+            ],
+          );
+        }),
+      );
+    }
+  }
+
+  menuList(BuildContext context){
+    final prov = Provider.of<Pertemuan11Provider>(context);
+    return PopupMenuButton(
+      icon: const Icon(Icons.more_vert),
+      itemBuilder: (BuildContext context){
+        return <PopupMenuEntry> [
+          PopupMenuItem(
+            child: ListTile(
+              onTap: () => prov.ubahList('hp'),
+              leading: const Icon(Icons.phone),
+              title: const Text('HP'),
+            )
+          ),
+          const PopupMenuDivider(),
+          PopupMenuItem(
+            child: ListTile(
+              onTap: () => prov.ubahList('laptop'),
+              leading: const Icon(Icons.laptop),
+              title: const Text('Laptop'),
+            )
+          ),
+        ];
+      }
+    );
+  }
+}
