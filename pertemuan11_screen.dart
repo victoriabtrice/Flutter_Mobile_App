96db79d880293a1c7deac1f2035b58e616eca20e
victoriabtrice <114051741+victoriabtrice@users.noreply.github.com>
2023-06-12 16:13:12 +0700

    Update pertemuan11_screen.dart

--- pertemuan11_screen.dart
+++ pertemuan11_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pertemuan11/pertemuan11_provider.dart';
+import 'package:flutter_application_11_latihan/pertemuan11/pertemuan11_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
-
 
 class Pertemuan11Screen extends StatefulWidget {
   const Pertemuan11Screen({super.key});
@@ -13,8 +12,9 @@ class Pertemuan11Screen extends StatefulWidget {
 
 class _Pertemuan11ScreenState extends State<Pertemuan11Screen> {
   @override
-  void initState(){
-    Future.microtask(() => Provider.of<Pertemuan11Provider>(context, listen: false).initialData());
+  void initState() {
+    Future.microtask(() =>
+        Provider.of<Pertemuan11Provider>(context, listen: false).initialData());
     super.initState();
   }
 
@@ -23,72 +23,75 @@ class _Pertemuan11ScreenState extends State<Pertemuan11Screen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pertemuan 11'),
-        actions: [
-          menuList(context)
-        ],
+        actions: [menuList(context)],
       ),
       body: body(context),
     );
   }
 
-  body(BuildContext context){
+  body(BuildContext context) {
     final prov = Provider.of<Pertemuan11Provider>(context);
-    if(prov.data == null){
+    if (prov.data == null) {
       return const CircularProgressIndicator();
-    }
-    else {
-      return ListView(
-        children: List.generate(prov.data['data']!.length, (index){
-          var item = prov.data['data']![index];
-          return Column(
-            children: [
-              ListTile(
-                leading: CircleAvatar(backgroundImage: NetworkImage(item['img'])),
-                title: Text(item['model']),
-                isThreeLine: true,
-                subtitle: Text("by " + item['developer'] + "\nprice : " + NumberFormat.currency(locale: 'ID').format(item['price']) + "\nrate : " + item['rating'].toString()),
+    } else {
+      return Container(
+        child: prov.data['data']!.length == 0
+            ? const Center(
+                child: Text('Data tidak ditemukan'),
+              )
+            : ListView(
+                children: List.generate(prov.data['data']!.length, (index) {
+                  var item = prov.data['data']![index];
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                            backgroundImage: NetworkImage(item['img'])),
+                        title: Text(item['model']),
+                        isThreeLine: true,
+                        subtitle: Text(
+                            "${"by " + item['developer']}\nprice : ${NumberFormat.currency(locale: 'ID').format(item['price'])}\nrate : ${item['rating']}"),
+                      ),
+                      const Divider()
+                    ],
+                  );
+                }),
               ),
-              const Divider()
-            ],
-          );
-        }),
       );
     }
   }
 
-  menuList(BuildContext context){
+  menuList(BuildContext context) {
     final prov = Provider.of<Pertemuan11Provider>(context);
     return PopupMenuButton(
-      icon: const Icon(Icons.more_vert),
-      itemBuilder: (BuildContext context){
-        return <PopupMenuEntry> [
-          PopupMenuItem(
-            child: ListTile(
+        icon: const Icon(Icons.more_vert),
+        itemBuilder: (BuildContext context) {
+          return <PopupMenuEntry>[
+            PopupMenuItem(
+                child: ListTile(
               onTap: () => prov.ubahList('hp'),
               leading: const Icon(Icons.phone),
               title: const Text('HP'),
-            )
-          ),
-          const PopupMenuDivider(),
-          PopupMenuItem(
-            child: ListTile(
+            )),
+            const PopupMenuDivider(),
+            PopupMenuItem(
+                child: ListTile(
               onTap: () => prov.ubahList('laptop'),
               leading: const Icon(Icons.laptop),
               title: const Text('Laptop'),
-            )
-          ),
-          const PopupMenuDivider(),
-          PopupMenuItem(
-            child: ListTile(
-              onTap: () { 
-                
+            )),
+            const PopupMenuDivider(),
+            PopupMenuItem(
+                child: ListTile(
+              onTap: () {
+                setState(() {
+                  prov.data['data']!.clear();
+                });
               },
               leading: const Icon(Icons.delete),
               title: const Text('Clear'),
-            )
-          ),
-        ];
-      }
-    );
+            )),
+          ];
+        });
   }
 }
